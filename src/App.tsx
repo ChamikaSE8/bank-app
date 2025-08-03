bea70ace7663060197d1694a295e0ae0fc8585e0
ChamikaSE8 <145045924+ChamikaSE8@users.noreply.github.com>
2025-08-03 22:13:36 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to the Bank App</h1>
-        <p>Your one-stop solution for all banking needs. I love you mage pana</p>
+        <p>Your one-stop solution for all banking needs.</p>
       </header>
     </div>
   );
